36fddf8606471b319c6f9402be476e1b9c9cd1fb
Oleg Plaxa <plaxaoleg@gmail.com>
2019-12-19 23:02:09 +0300

    fix dockerfile1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 wp core download --force --allow-root
 wp config create \
-  --dbhost=$WORDPRESS_DB_HOST \
+  --dbhost=db \
   --dbname=$WORDPRESS_DB_NAME \
   --dbuser=$WORDPRESS_DB_USER \
   --dbpass=$WORDPRESS_DB_PASSWORD \
